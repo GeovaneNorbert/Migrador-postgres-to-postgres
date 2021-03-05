45b390a8792613b9c9c349269c9fb1625f82cf6e
Geovane <geovane.norbert@gmail.com>
2021-03-05 15:25:16 -0300

    conectando no banco

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -22,35 +22,240 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Empresa = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Repeticoes = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Linhas = New System.Windows.Forms.TextBox()
+        Me.Log = New System.Windows.Forms.RichTextBox()
+        Me.Iniciar = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.database = New System.Windows.Forms.TextBox()
+        Me.Senha = New System.Windows.Forms.TextBox()
+        Me.Usuario = New System.Windows.Forms.TextBox()
+        Me.Servidor = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'Label11
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(362, 133)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(55, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "hello word"
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(357, 89)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(13, 13)
+        Me.Label11.TabIndex = 43
+        Me.Label11.Text = "0"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(244, 89)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(103, 13)
+        Me.Label10.TabIndex = 42
+        Me.Label10.Text = "Tempo de operação"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(244, 57)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(97, 13)
+        Me.Label9.TabIndex = 41
+        Me.Label9.Text = "Inicio da Operação"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(357, 57)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(13, 13)
+        Me.Label8.TabIndex = 40
+        Me.Label8.Text = "0"
+        '
+        'Empresa
+        '
+        Me.Empresa.Location = New System.Drawing.Point(121, 206)
+        Me.Empresa.Name = "Empresa"
+        Me.Empresa.Size = New System.Drawing.Size(100, 20)
+        Me.Empresa.TabIndex = 39
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(39, 213)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(47, 13)
+        Me.Label7.TabIndex = 38
+        Me.Label7.Text = "empresa"
+        '
+        'Repeticoes
+        '
+        Me.Repeticoes.Location = New System.Drawing.Point(121, 287)
+        Me.Repeticoes.Name = "Repeticoes"
+        Me.Repeticoes.Size = New System.Drawing.Size(100, 20)
+        Me.Repeticoes.TabIndex = 37
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(34, 290)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 13)
+        Me.Label6.TabIndex = 36
+        Me.Label6.Text = "Repetições"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(11, 244)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 13)
+        Me.Label5.TabIndex = 35
+        Me.Label5.Text = "Linhas por execução"
+        '
+        'Linhas
+        '
+        Me.Linhas.Location = New System.Drawing.Point(121, 241)
+        Me.Linhas.Name = "Linhas"
+        Me.Linhas.Size = New System.Drawing.Size(100, 20)
+        Me.Linhas.TabIndex = 34
+        '
+        'Log
+        '
+        Me.Log.Location = New System.Drawing.Point(500, 6)
+        Me.Log.Name = "Log"
+        Me.Log.Size = New System.Drawing.Size(290, 438)
+        Me.Log.TabIndex = 33
+        Me.Log.Text = ""
+        '
+        'Iniciar
+        '
+        Me.Iniciar.Location = New System.Drawing.Point(281, 196)
+        Me.Iniciar.Name = "Iniciar"
+        Me.Iniciar.Size = New System.Drawing.Size(75, 23)
+        Me.Iniciar.TabIndex = 32
+        Me.Iniciar.Text = "Iniciar"
+        Me.Iniciar.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(49, 175)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 13)
+        Me.Label4.TabIndex = 31
+        Me.Label4.Text = "base"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(46, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 30
+        Me.Label3.Text = "senha"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(362, 175)
+        Me.Label2.Location = New System.Drawing.Point(44, 85)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(55, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "hello word"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 29
+        Me.Label2.Text = "usuario"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 28
+        Me.Label1.Text = "servidor"
+        '
+        'database
+        '
+        Me.database.Location = New System.Drawing.Point(121, 168)
+        Me.database.Name = "database"
+        Me.database.Size = New System.Drawing.Size(100, 20)
+        Me.database.TabIndex = 27
+        '
+        'Senha
+        '
+        Me.Senha.Location = New System.Drawing.Point(121, 121)
+        Me.Senha.Name = "Senha"
+        Me.Senha.Size = New System.Drawing.Size(100, 20)
+        Me.Senha.TabIndex = 26
+        '
+        'Usuario
+        '
+        Me.Usuario.Location = New System.Drawing.Point(121, 82)
+        Me.Usuario.Name = "Usuario"
+        Me.Usuario.Size = New System.Drawing.Size(100, 20)
+        Me.Usuario.TabIndex = 25
+        '
+        'Servidor
+        '
+        Me.Servidor.Location = New System.Drawing.Point(121, 42)
+        Me.Servidor.Name = "Servidor"
+        Me.Servidor.Size = New System.Drawing.Size(100, 20)
+        Me.Servidor.TabIndex = 24
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(419, 196)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 23
+        Me.Button2.Text = "Cancelar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(121, 367)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 22
+        Me.Button1.Text = "Conectar"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Empresa)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Repeticoes)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Linhas)
+        Me.Controls.Add(Me.Log)
+        Me.Controls.Add(Me.Iniciar)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.database)
+        Me.Controls.Add(Me.Senha)
+        Me.Controls.Add(Me.Usuario)
+        Me.Controls.Add(Me.Servidor)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -58,6 +263,26 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Label1 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Empresa As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Repeticoes As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Linhas As TextBox
+    Friend WithEvents Log As RichTextBox
+    Friend WithEvents Iniciar As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents database As TextBox
+    Friend WithEvents Senha As TextBox
+    Friend WithEvents Usuario As TextBox
+    Friend WithEvents Servidor As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
 End Class
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,3 +1,202 @@
-﻿Public Class Form1
+﻿
+Imports Npgsql
+Imports System.Data
+Imports System.Security
+Imports System.IO
+Imports System.Text
+Public Class Form1
+    Dim conn As NpgsqlConnection
+    Dim chaveErro As String
+    Dim Conexao As String
+    Dim StrLog As String
+    Dim NoLinhas As String
+    Dim msg As MessageBox
+    Dim NoRepeticoes As Integer
+    Dim StrEmpresa As String
+    Dim cronometro As New Stopwatch
 
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Servidor.Text = "localhost"
+        Senha.PasswordChar = "*"
+        Usuario.Text = "postgres"
+        Senha.Text = "postgres"
+        database.Text = "portas"
+        Linhas.Text = "1"
+        Repeticoes.Text = "10"
+        chaveErro = "0"
+        Empresa.Text = "ep001"
+        Label8.Text = "0"
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Conexao = "Server=" + Servidor.Text + ";Port=5432;UserId=" + Usuario.Text + ";Password=" + Senha.Text + ";Database=" + database.Text & ";Timeout=1024;CommandTimeout=3600;"
+        StrEmpresa = Empresa.Text
+
+        conn = New NpgsqlConnection(Conexao)
+        Try
+            conn.Open()
+            If (conn.State = ConnectionState.Open) Then
+                MessageBox.Show(“Conexão bem sucedida!”)
+            Else
+                MessageBox.Show("nao abriu")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("falha na conexao")
+
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Iniciar.Click
+
+        Label8.Text = DateTime.Now.ToString()
+        Label11.Text = "0"
+        NoRepeticoes = Repeticoes.Text
+        NoLinhas = Linhas.Text
+        cronometro.Start()
+        Dim i As Integer
+        i = 1
+        While i <= NoRepeticoes
+            If i > 1 Then
+                Dim myProcess As New Process
+                Label11.Text = cronometro.Elapsed.ToString
+                Dim result As DialogResult = MessageBox.Show("Deseja continuar", "Deletando", MessageBoxButtons.YesNoCancel)
+                If result = DialogResult.Cancel Then
+                    Exit While
+                ElseIf result = DialogResult.No Then
+                    Exit While
+                ElseIf result = DialogResult.Yes Then
+
+                End If
+            End If
+            deleta()
+            i = i + 1
+        End While
+        StrLog = StrLog & " CONCLUIDO " & vbCrLf
+        Log.Text = StrLog
+        Log.Refresh()
+        cronometro.Stop()
+        Label11.Text = cronometro.Elapsed.ToString
+        cronometro.Reset()
+    End Sub
+
+    Private Sub deleta()
+        Dim Sql As String = "begin; "
+        Sql = Sql & " delete FROM " & StrEmpresa & ".es_tbsaldos  "
+        Sql = Sql & "using " & StrEmpresa & ".es_tbsaldos tb  "
+        Sql = Sql & "left join  "
+        Sql = Sql & "(  "
+        Sql = Sql & "select PRODUT_pdi,sgrupo_pdI from " & StrEmpresa & ".Pd_TblItens "
+        Sql = Sql & "UNION "
+        Sql = Sql & "SELECT PRODUT_PRI,SGRUPO_PRI FROM " & StrEmpresa & ".PR_ITPEDIDO "
+        Sql = Sql & "UNION "
+        Sql = Sql & "select produt_ifa, sequen_ifa from ep001.pd_itfabric "
+        Sql = Sql & "UNION "
+        Sql = Sql & "SELECT PRODUT_ENT,SEQUEN_ENT FROM " & StrEmpresa & ".TITENTRA) as vendas "
+        Sql = Sql & "on produt_pdi=tb.codigo_sld and sgrupo_pdi=tb.sequen_sld "
+        Sql = Sql & "where produt_pdi is null "
+        Sql = Sql & "and tb.mchave_sld=" & StrEmpresa & ".es_tbsaldos.mchave_sld "
+        Sql = Sql & "and tb.mchave_sld >" & chaveErro
+        Sql = Sql & "and tb.mchave_sld<= "
+        Sql = Sql & "(select max(mchave_sld) from ( "
+        Sql = Sql & "select mchave_sld FROM " & StrEmpresa & ".es_tbsaldos tb "
+        Sql = Sql & "left join "
+        Sql = Sql & "( "
+        Sql = Sql & "select PRODUT_pdi,sgrupo_pdI from " & StrEmpresa & ".Pd_TblItens "
+        Sql = Sql & "UNION "
+        Sql = Sql & "SELECT PRODUT_PRI,SGRUPO_PRI FROM " & StrEmpresa & ".PR_ITPEDIDO "
+        Sql = Sql & "UNION "
+        Sql = Sql & "select produt_ifa, sequen_ifa from ep001.pd_itfabric "
+        Sql = Sql & "UNION "
+        Sql = Sql & "SELECT PRODUT_ENT,SEQUEN_ENT FROM " & StrEmpresa & ".TITENTRA) as vendas "
+        Sql = Sql & "on produt_pdi=tb.codigo_sld and sgrupo_pdi=tb.sequen_sld "
+        Sql = Sql & "where produt_pdi is null "
+        Sql = Sql & "and mchave_sld >" & chaveErro
+        Sql = Sql & "order by mchave_sld "
+        Sql = Sql & "limit " & NoLinhas & " ) as a); commit; "
+
+        Try
+            If (conn.State = ConnectionState.Closed) Then
+                conn.Open()
+            End If
+
+            Dim Command As NpgsqlCommand = New NpgsqlCommand(Sql, conn)
+            Command.ExecuteNonQuery()
+            StrLog = StrLog & "" & NoLinhas & " LINHAS EXECUTADAS " & vbCrLf
+            Log.Text = StrLog
+            Log.Refresh()
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            chaveErro = (PegaMaiorChave())
+            StrLog = StrLog & "Erro ao executar o bloco de " & NoLinhas & " linhas onde o maior registro é " & chaveErro & "" & vbCrLf
+            Log.Text = StrLog
+            Log.Refresh()
+        Finally
+
+        End Try
+    End Sub
+
+    Public Function PegaMaiorChave() As String
+        Dim ConsultaMaiorChave As NpgsqlConnection
+        Dim ds As New DataSet()
+        Dim da As NpgsqlDataAdapter
+        Dim StringSQL As String
+        ConsultaMaiorChave = New NpgsqlConnection(Conexao)
+        Try
+            ConsultaMaiorChave.Open()
+
+            StringSQL = "select max(mchave_sld) from ( "
+            StringSQL = StringSQL & " select mchave_sld FROM " & StrEmpresa & ".es_tbsaldos tb "
+            StringSQL = StringSQL & " left join "
+            StringSQL = StringSQL & "( "
+            StringSQL = StringSQL & " select PRODUT_pdi,sgrupo_pdI from " & StrEmpresa & ".Pd_TblItens "
+            StringSQL = StringSQL & " UNION "
+            StringSQL = StringSQL & " SELECT PRODUT_PRI,SGRUPO_PRI FROM " & StrEmpresa & ".PR_ITPEDIDO "
+            StringSQL = StringSQL & " UNION "
+            StringSQL = StringSQL & " select produt_ifa, sequen_ifa from ep001.pd_itfabric "
+            StringSQL = StringSQL & " UNION "
+            StringSQL = StringSQL & " SELECT PRODUT_ENT,SEQUEN_ENT FROM " & StrEmpresa & ".TITENTRA) as vendas "
+            StringSQL = StringSQL & " on produt_pdi=tb.codigo_sld and sgrupo_pdi=tb.sequen_sld "
+            StringSQL = StringSQL & " where produt_pdi is null "
+            StringSQL = StringSQL & " and mchave_sld>" & chaveErro
+            StringSQL = StringSQL & " order by mchave_sld "
+            StringSQL = StringSQL & " limit " & NoLinhas & " ) as a "
+            da = New NpgsqlDataAdapter(StringSQL, ConsultaMaiorChave)
+
+            da.Fill(ds)
+
+            Return ds.Tables(0).Rows(0)(0)
+
+        Catch ex As Exception
+
+            MsgBox("abra a conexao primeiro")
+
+            Return ""
+        Finally
+            If ConsultaMaiorChave.State = ConnectionState.Open Then
+                ConsultaMaiorChave.Close()
+            End If
+        End Try
+    End Function
+    Private Sub AbreConfirmacao()
+
+    End Sub
+
+
+
+    Private Sub Log_TextChanged(sender As Object, e As EventArgs) Handles Log.TextChanged
+        Log.SelectionStart = Log.Text.Length
+        Log.ScrollToCaret()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (conn.State = ConnectionState.Open) Then
+            conn.Close()
+        End If
+    End Sub
 End Class
+
+
